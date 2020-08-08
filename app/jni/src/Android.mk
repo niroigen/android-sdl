2cc5fc071833e6b8d5373fe1e68536cfcfac86eb
Niroigen Suntharam <niroigen@gmail.com>
2020-08-08 01:32:47 -0400

    First commit

---
+++ app/jni/src/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+
+# Add your application source files here...
+LOCAL_SRC_FILES := 52_hello_mobile.cpp
+
+LOCAL_SHARED_LIBRARIES := SDL2
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+APP_STL := c++_shared
+
+include $(BUILD_SHARED_LIBRARY)
